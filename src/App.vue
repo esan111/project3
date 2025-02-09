1233ec53b1aa28d06ec80f441064e28748862710
Emanuel Sanchez <ejsanch07@gmail.com>
2025-02-09 01:20:11 -0600

    adapting body to template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Countdown Timer</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Countdown Timer</h1>
-        <label for="timeInput">Please input a timer (1-60 seconds):</label>
-        <input type="number" id="timeInput" min="1" max="60" inputmode="numeric">
-        <button id="startButton">Start Countdown</button>
-        <div id="timerDisplay">00:00</div>
-    </div>
-    <script src="scripts/script.js"></script>
-</body>
-</html>
+
+
+<template>
+  <div class="container">
+    <h1>Countdown Timer</h1>
+    <label for="timeInput">Please input a timer (1-60 seconds): </label>
+    <input type="number" id="timeInput" v-model="timeInput" min="1" max="60" inputmode="numeric"> 
+    <button @click="startCountdown">Start Countdown</button>
+    <div id="timerDisplay">{{ formattedTime }}</div>
+  </div>
+</template>
