a76e7d4453e806efc746b28840a657f7cc3a8da7
Emanuel Sanchez <ejsanch07@gmail.com>
2025-02-09 02:04:56 -0600

    Most of it figured out, from js to script, need 2 fix timer

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,43 @@
+<script setup>
+import './style.css';
+import { ref, computed, onUnmounted } from 'vue';
 
+const timeInput = ref(null);
+const timeLeft = ref(0);
+let countdownInterval;
+
+const formattedTime = computed(() => {
+  const minutes = Math.floor(timeLeft.value / 60);
+  const seconds = timeLeft.value % 60;
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+});
+
+const startCountdown = () => {
+  const time = parseInt(timeInput.value);
+
+  if (isNaN(time) || time < 1 || time > 60) {
+    alert("Please enter a VALID number between 1 and 60.");
+    return;
+  }
+
+  clearInterval(countdownInterval);
+
+  timeLeft.value = time;
+
+  countdownInterval = setInterval(() => {
+    timeLeft.value--;
+
+    if (time <= 0) {
+            clearInterval(countdownInterval);
+            timeLeft.value = 0;
+            alert("Time's up! Let's Go!");
+            return;
+        }
+  }, 1000);
+};
+
+
+</script>
 
 
 <template>
